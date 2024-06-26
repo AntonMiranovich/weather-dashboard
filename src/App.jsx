fb65ee7054bc9050303b9468daccfcf08ee4ef53
AntonMiranovich <antonmir992@gmail.com>
2024-06-27 00:26:05 +0300

    add task

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Search from './components/SearchForm'
+
+function App() {
+
+
+  return (
+    <>
+      <Search />
+      <h1>Hello</h1>
+    </>
+  )
+}
+
+export default App
